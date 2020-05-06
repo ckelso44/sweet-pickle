f04694a8e080ccea6cfb04323f4519dd5fa5d9ce
ckelso44 <ckelso44@gmail.com>
2020-05-05 19:51:56 -0700

    Completed Details section on tasks

--- JS/tasks.js
+++ JS/tasks.js
@@ -1,38 +1,61 @@
+//Test Data
 var startDay = "Monday"
     // Designed to load to current page of the week
 
-function loadMainPage() {
-    var defaultValues = {
-        dayOfWeek: "Tuesday",
-        sheets: 6,
-        sheeteComplete: 4,
-        CPCExpected: 70.5,
-        CPCActual: 86,
-        creditExpected: 1000,
-        creditActual: 962.45,
-        debitExpected: 847,
-        debitActual: 911.12
-    }
-    document.getElementById("dayOfWeek").innerHTML = defaultValues["dayOfWeek"];
+var testSumValues = {
+    dayOfWeek: "Tuesday",
+    sheets: 6,
+    sheetsComplete: 4,
+    CPCExpected: 70.5,
+    CPCActual: 86,
+    creditExpected: 1000,
+    creditActual: 962.45,
+    debitExpected: 847,
+    debitActual: 911.12
+}
 
-    document.getElementById("CPCExpected").innerHTML = CurrencyFormatted(defaultValues["CPCExpected"]);
-    document.getElementById("CPCActual").innerHTML = CurrencyFormatted(defaultValues["CPCActual"]);
-    var calcCPCDiff = defaultValues["CPCActual"] - defaultValues["CPCExpected"];
+function loadMainPage() {
+    //Populate the main page
+    calcSummary(testSumValues);
+}
+
+function calcSummary(sumValues) {
+
+    var calcCPCDiff = sumValues["CPCActual"] - sumValues["CPCExpected"];
+    var calcCreditDiff = sumValues["creditActual"] - sumValues["creditExpected"];
+    var calcDebitDiff = sumValues["debitActual"] - sumValues["debitExpected"];
+    var budgetSum = sumValues["CPCExpected"] + sumValues["creditExpected"] + sumValues["debitExpected"];
+    var actualSum = sumValues["CPCActual"] + sumValues["creditActual"] + sumValues["debitActual"];
+    var diffSum = calcCPCDiff + calcCreditDiff + calcDebitDiff;
+    var gstSum = actualSum * 0.05;
+    var pstSum = actualSum * 0.07;
+    var netSum = gstSum + pstSum + actualSum
+    var netProfit = netSum - budgetSum;
+
+    //Title Bar
+    document.getElementById("dayOfWeek").innerHTML = sumValues["dayOfWeek"];
+    document.getElementById("sumProfit").innerHTML = CurrencyFormatted(netProfit);
+    //Totals Sheet
+    document.getElementById("CPCExpected").innerHTML = CurrencyFormatted(sumValues["CPCExpected"]);
+    document.getElementById("CPCActual").innerHTML = CurrencyFormatted(sumValues["CPCActual"]);
     document.getElementById("CPCDiff").innerHTML = CurrencyFormatted(calcCPCDiff);
 
-    document.getElementById("creditExpected").innerHTML = CurrencyFormatted(defaultValues["creditExpected"]);
-    document.getElementById("creditActual").innerHTML = CurrencyFormatted(defaultValues["creditActual"]);
-    var calcCreditDiff = defaultValues["creditActual"] - defaultValues["creditExpected"];
+    document.getElementById("creditExpected").innerHTML = CurrencyFormatted(sumValues["creditExpected"]);
+    document.getElementById("creditActual").innerHTML = CurrencyFormatted(sumValues["creditActual"]);
     document.getElementById("creditDiff").innerHTML = CurrencyFormatted(calcCreditDiff);
 
-    document.getElementById("debitExpected").innerHTML = CurrencyFormatted(defaultValues["debitExpected"]);
-    document.getElementById("debitActual").innerHTML = CurrencyFormatted(defaultValues["debitActual"]);
-    var calcDebitDiff = defaultValues["debitActual"] - defaultValues["debitExpected"];
+    document.getElementById("debitExpected").innerHTML = CurrencyFormatted(sumValues["debitExpected"]);
+    document.getElementById("debitActual").innerHTML = CurrencyFormatted(sumValues["debitActual"]);
     document.getElementById("debitDiff").innerHTML = CurrencyFormatted(calcDebitDiff);
 
-    document.getElementById("actualSum").innerHTML =
-        CurrencyFormatted(defaultValues["CPCActual"] + defaultValues["creditActual"] + defaultValues["debitActual"]);
-    document.getElementById("diffSum").innerHTML =
-        CurrencyFormatted(calcCPCDiff + calcCreditDiff + calcDebitDiff);
+    document.getElementById("actualSum").innerHTML = CurrencyFormatted(actualSum);
+    document.getElementById("diffSum").innerHTML = CurrencyFormatted(diffSum);
+
+    //Details Sheet
+    document.getElementById("GST").innerHTML = CurrencyFormatted(gstSum);
+    document.getElementById("PST").innerHTML = CurrencyFormatted(pstSum);
+    document.getElementById("budget").innerHTML = CurrencyFormatted(budgetSum);
+    document.getElementById("netTotal").innerHTML = CurrencyFormatted(netSum);
+
 
 }
